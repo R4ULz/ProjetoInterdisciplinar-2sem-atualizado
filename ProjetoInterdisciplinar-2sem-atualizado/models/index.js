278fb26a97389ef488a14d5c1ef390fb8ab59025
Leandro Rodrigues <78665194+LeandroRodrigues061@users.noreply.github.com>
2024-06-08 02:24:04 -0300

    Add files via upload
    Commit e abriçssão de branch nova 08/06. Nada quebrado. Tudo funcionando. Algumas partes do código apenas faltam terminar.

---
+++ ProjetoInterdisciplinar-2sem-atualizado/models/index.js
@@ -0,0 +1,47 @@
+const db = require('./banco'); // ajuste o caminho conforme necessário
+
+// Importando modelos
+const User = require('./User');
+const Pedido = require('./pedido');
+const Produto = require('./produto');
+const Pedido_Produto = require('./pedido_produto');
+
+// Definindo relacionamentos
+User.hasMany(Pedido, {
+    constraint: true,
+    foreignKey: 'UserId'});
+Pedido.belongsTo(User, {
+    constraint: true,
+    foreignKey: 'UserId'});
+
+Produto.belongsToMany(Pedido, { 
+  through: Pedido_Produto,
+  foreignKey: 'ProdutoId',
+  otherKey: 'PedidoId'
+ });
+Pedido.belongsToMany(Produto, { 
+    through: Pedido_Produto,
+    foreignKey: 'PedidoId',  // Chave estrangeira para Pedido em Pedido_Produto
+    otherKey: 'ProdutoId'    // Chave estrangeira para Produto em Pedido_Produto
+});
+
+
+Pedido.hasMany(Pedido_Produto, { foreignKey: 'PedidoId' });
+Pedido_Produto.belongsTo(Pedido, {
+  foreignKey: 'PedidoId'  // Confirma que a chave estrangeira na tabela 'Pedido_Produto' é 'PedidoId'
+});
+
+Produto.hasMany(Pedido_Produto, {
+  foreignKey: 'ProdutoId'
+});
+Pedido_Produto.belongsTo(Produto, {
+  foreignKey: 'ProdutoId'
+});
+
+module.exports = {
+  db,
+  User,
+  Pedido,
+  Produto,
+  Pedido_Produto
+};
---
+++ ProjetoInterdisciplinar-2sem-atualizado/models/produto.js
@@ -0,0 +1,34 @@
+const db = require("./banco")
+const Pedido = require('./pedido');
+const Pedido_Produto = require('./pedido_produto');
+
+const Produtos = db.sequelize.define("produtos",{
+    ProdutoId:{
+        type: db.Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    imagem:{
+        type: db.Sequelize.STRING,
+        allowNull:false
+    },
+    nome:{
+        type: db.Sequelize.STRING,
+        allowNull: false
+    },
+    valor:{
+        type: db.Sequelize.DOUBLE,
+        allowNull: false
+    },
+    descricao:{
+        type: db.Sequelize.STRING,
+        allowNull: false
+    },
+    categoria:{
+        type: db.Sequelize.STRING,
+        allowNull: false
+    },
+})
+
+
+module.exports = Produtos
