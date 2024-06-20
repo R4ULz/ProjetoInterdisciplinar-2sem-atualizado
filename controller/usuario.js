7626b23586ba0223463c1c58e4d030e30baf4734
Leandro Rodrigues <78665194+LeandroRodrigues061@users.noreply.github.com>
2024-06-20 09:15:19 -0300

    Commit 20/06
    Commit

--- controller/usuario.js
+++ controller/usuario.js
@@ -3,37 +3,31 @@ const model = require("../models");
 const Users = model.Users;
 
 module.exports = {
-    async criarUsuario(req,res){
-        
-        try{
-            const {
-                nome,
-                email,
-                passHash,
-                endereco,
-                telefone
-            } = req.body;
+  async criarUsuario(req, res) {
+    try {
+      const { nome, email, passHash, endereco, telefone } = req.body;
 
-            const existeEmail = await model.users.findAll({where: {email : req.body.email}})
-            
-            if(existeEmail){
-                return res.render('signin', {
-                  message: 'Esse email está em uso!'
-                  })
-            }else{
-                const SalvarUsuario = await Users.create({
-                    nome,
-                    email,
-                    passHash,
-                    endereco,
-                    telefone
-                });
-                SalvarUsuario.senha_hash = undefined;
-                return res.redirect('/login');
-            }
-        }catch(err){
-            return res.send("Erro ao cadastrar usuário" + err);
-        }
+      const existeEmail = await model.users.findAll({
+        where: { email: req.body.email },
+      });
+
+      if (existeEmail) {
+        return res.render("signin", {
+          message: "Esse email está em uso!",
+        });
+      } else {
+        const SalvarUsuario = await Users.create({
+          nome,
+          email,
+          passHash,
+          endereco,
+          telefone,
+        });
+        SalvarUsuario.senha_hash = undefined;
+        return res.redirect("/login");
+      }
+    } catch (err) {
+      return res.send("Erro ao cadastrar usuário" + err);
     }
-
-}
+  },
+};
