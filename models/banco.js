4f60e2ce1c0e2142b54fb22b8476ec5469a57a46
RAUL DOS SANTOS ARAUJO <122099251+R4ULz@users.noreply.github.com>
2024-09-05 11:05:24 -0300

    sada

--- models/banco.js
+++ models/banco.js
@@ -1,17 +1,26 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const DB_HOST = process.env.DB_HOST;
+const DB_PORT = process.env.DB_PORT || 5432; // Porta padrão do PostgreSQL
+const DB_NAME = process.env.DB_NAME;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+    host: DB_HOST,
+    port: DB_PORT,
     dialect: 'postgres',
-    logging: false
+    logging: false // Desative o logging SQL se não for necessário
 });
 
+// Verifique a conexão
 sequelize.authenticate().then(() => {
     console.log('Banco de dados conectado com sucesso');
-}).catch((erro) => {
-    console.log('Falha ao conectar: ' + erro);
+}).catch((err) => {
+    console.error('Erro ao conectar ao banco de dados:', err);
 });
 
 module.exports = {
-    Sequelize: Sequelize,
-    sequelize: sequelize
+    Sequelize,
+    sequelize
 };
